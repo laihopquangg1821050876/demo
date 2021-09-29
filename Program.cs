b7d13a36a58b1041ea40ed33cd8350824a7a21a4
quanga123 <quangoc0302@gmail.com>
2021-09-29 08:19:43 +0700

    quanggg

--- Program.cs
+++ Program.cs
@@ -14,10 +14,8 @@ namespace demo
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+       
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
